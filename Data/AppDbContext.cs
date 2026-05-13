e5724a7a1704fb5e0f74a71025561cc8bb3a43fb
Megi Ahmetaj <megiahmetaj2005@gmail.com>
2026-05-13 23:13:32 +0200

    new change

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie.API.Models;
+
+namespace Movie.API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Models.Movie> Movies { get; set; }
+}
