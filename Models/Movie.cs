622ec2caf5406a12d263d953287790492e179d00
Megi Ahmetaj <megiahmetaj2005@gmail.com>
2026-05-13 23:12:50 +0200

    adding Movie.cs

---
+++ Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace Movie.API.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public string Director { get; set; } = string.Empty;
+    public int Year { get; set; }
+    public double Rating { get; set; }
+    public int Duration { get; set; }
+    public string PosterUrl { get; set; } = string.Empty;
+}
